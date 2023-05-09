7188880ad85b38001df54599e8b8709f3984d327
3rsservices <101308871+minhnguyen200703@users.noreply.github.com>
2023-05-09 11:04:37 +0700

    Update

--- src/test/java/Model/TestProduct.java
+++ src/test/java/Model/TestProduct.java
@@ -1,76 +1,65 @@
-//package ShoppingService;
-//
-///**
-// * @author Nguyen Nhat Minh - s3932112
-// */
-//
-//import org.junit.jupiter.api.*;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class TestProduct {
-//
-//    @Test
-//    void testProductCreation() {
-//        Product product = new PhysicalProduct("TestProduct", "Test description", 10, 19.99, 1.0);
-//        assertEquals("TestProduct", product.getName());
-//        assertEquals("Test description", product.getDescription());
-//        assertEquals(10, product.getQuantityAvailable());
-//        assertEquals(19.99, product.getPrice(), 0.01);
-//    }
-//
-//    @Test
-//    void testSetQuantityAvailableWithString() {
-//        Product product = new PhysicalProduct("testSetQuantityAvailableWithString", "Test description", 10, 19.99, 1.0);
-//        product.setQuantityAvailable("5");
-//        assertEquals(5, product.getQuantityAvailable());
-//    }
-//
-//    @Test
-//    void testSetQuantityAvailableWithInvalidString() {
-//        Product product = new PhysicalProduct("testSetQuantityAvailableWithInvalidString", "Test description", 10, 19.99, 1.0);
-//        product.setQuantityAvailable(5);
-//        assertEquals(5, product.getQuantityAvailable());
-//    }
-//
-//    @Test
-//    void testCheckIfNameExisted() {
-//        Product product = new PhysicalProduct("testCheckIfNameExisted", "Test description", 10, 19.99, 1.0);
-//        assertTrue(Product.checkIfNameExisted("testCheckIfNameExisted"));
-//        assertFalse(Product.checkIfNameExisted("NonExistingProduct"));
-//    }
-//
-//    @Test
-//    void testGetProduct() {
-//        Product product = new PhysicalProduct("testGetProduct", "Test description", 10, 19.99, 1.0);
-//        assertEquals(product, Product.getProduct("testGetProduct"));
-//        assertNull(Product.getProduct("NonExistingProduct"));
-//    }
-//
-//    @Test
-//    public void testUpdateDigitalProductInformation() {
-//        DigitalProduct product = new DigitalProduct("testUpdateDigitalProductInformation", "Description", 10, 20.0);
-//        product.setName("NewName");
-//        product.setDescription("NewDescription");
-//        product.setPrice(30.0);
-//        assertEquals("NewName", product.getName());
-//        assertEquals("NewDescription", product.getDescription());
-//        assertEquals(30.0, product.getPrice(), 0.0);
-//    }
-//
-//    @Test
-//    public void testUpdatePhysicalProductInformation() {
-//        PhysicalProduct product = new PhysicalProduct("testUpdatePhysicalProductInformation", "Description", 10, 20.0, 5.0);
-//        product.setName("NewName");
-//        product.setDescription("NewDescription");
-//        product.setPrice(30.0);
-//        product.setWeight(10.0);
-//        assertEquals("NewName", product.getName());
-//        assertEquals("NewDescription", product.getDescription());
-//        assertEquals(30.0, product.getPrice(), 0.0);
-//        assertEquals(10.0, product.getWeight(),0.0);
-//    }
-//
-//
-//}
-//
+package Model;
+
+import ShoppingService.Controller.ProductController;
+import ShoppingService.Model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestProduct {
+
+    private Product physicalProduct;
+    private Product digitalProduct;
+
+    @BeforeEach
+    public void setUp() {
+        physicalProduct = new PhysicalProduct("Physical Product", "This is a physical product", 10, 100.0, 5.0, true, "FreeTaxRate");
+        digitalProduct = new DigitalProduct("Digital Product", "This is a digital product", 5, 50.0, false, "FreeTaxRate");
+    }
+
+    @Test
+    public void testGetProductName() {
+        assertEquals("Physical Product", physicalProduct.getName());
+        assertEquals("Digital Product", digitalProduct.getName());
+    }
+
+    @Test
+    public void testGetProductDescription() {
+        assertEquals("This is a physical product", physicalProduct.getDescription());
+        assertEquals("This is a digital product", digitalProduct.getDescription());
+    }
+
+    @Test
+    public void testGetQuantityAvailable() {
+        assertEquals(10, physicalProduct.getQuantityAvailable());
+        assertEquals(5, digitalProduct.getQuantityAvailable());
+    }
+
+    @Test
+    public void testGetPrice() {
+        assertEquals(100.0, physicalProduct.getPrice());
+        assertEquals(50.0, digitalProduct.getPrice());
+    }
+
+    @Test
+    public void testIsGiftable() {
+        assertTrue(physicalProduct.isGiftable());
+        assertFalse(digitalProduct.isGiftable());
+    }
+
+    @Test
+    public void testGetTaxType() {
+        assertEquals("FreeTaxRate", physicalProduct.getTaxType());
+        assertEquals("FreeTaxRate", digitalProduct.getTaxType());
+    }
+
+    @Test
+    public void testGetType() {
+        assertEquals("PHYSICAL", physicalProduct.getType());
+        assertEquals("DIGITAL", digitalProduct.getType());
+    }
+
+
+
+}
